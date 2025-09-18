5d7c8571aa343c3c5449f006974fe3696ee05520
Shem Irekpita <shem.irekpita@stu.ocsb.ca>
2025-09-18 17:38:25 +0000

    this is a calculation of arear of a rectangle in CPP

---
+++ areaPerRect.cpp
@@ -0,0 +1,15 @@
+// Created by: Shem
+// Created on: 9/17/2025
+// This program calculates and displays the area and
+// perimeter of a rectangle with dimensions 5cm x 3cm.
+// Copyright 2025 Shem
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "For a rectangle that has dimensions\n";
+    std::cout << "length 5cm and width 3cm:\n";
+    std::cout << "\n";
+    std::cout << "Area = " << 5 * 3 << " cm^2\n";
+    std::cout << "Perimeter = " << 2 * (5 + 3) << " cm\n";
+}
